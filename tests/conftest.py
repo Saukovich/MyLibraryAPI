3583d2708598f25a8f0d80b9c230c80b564aa4fe
Илья Сауков <iamsaukovich@gmail.com>
2026-04-23 20:51:06 +0300

    fix: enable foreign keys cheking

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,13 @@
 import asyncio
 
 import pytest
+from sqlalchemy import event
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from app.core.database import Model
 
 
-TEST_DATABASE_URL = "sqlite+aiosqlite:///:memory:"
+TEST_DATABASE_URL = "sqlite+aiosqlite:///:memory:?foreign_keys=on"
 
 engine = create_async_engine(TEST_DATABASE_URL)
 testing_session = async_sessionmaker(engine, expire_on_commit=False)
@@ -21,6 +22,19 @@ async def event_loop():
     loop = asyncio.get_event_loop_policy().new_event_loop()  # создание цикла событий
     yield loop
     loop.close()
+
+
+@event.listens_for(engine.sync_engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    """
+    Включение поддержки внешних ключей в SQLite.
+    :param dbapi_connection: Connection - соединение.
+    :param connection_record: ConnectionRecord - запись соединения.
+    :return: None.
+    """
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 
 @pytest.fixture(autouse=True, scope="function")
